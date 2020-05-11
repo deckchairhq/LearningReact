d2e6f7c9df6fbf9bae511df299afbc42142beb1c
Gareth E <mail@deckchair.com>
2020-05-11 13:30:53 +0100

    Fixing issues with Twitter Bootstrap not showing when deployed

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+// https://stackoverflow.com/questions/59777625/bootstrap-styling-not-showing-when-deploying-to-heroku-a-ror-application-with-re
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}))
+
 module.exports = environment
